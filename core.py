83516695ca2b169fd97768c1bdbff89021058d3f
ojromanSE <oscar@schaperintl.com>
2025-11-03 16:25:56 -0300

    Update core.py

--- core.py
+++ core.py
@@ -27,18 +27,15 @@ HEADER_COLUMN_MAPS = [
     {'WellName':'Well Name','County':'County/Parish','LateralLength':'DI Lateral Length',
      'PrimaryFormation':'Producing Reservoir','CompletionDate':'Completion Date',
      'FirstProdDate':'First Prod Date','State':'State','API10':'API10'}
+]
 PROD_COLUMN_MAPS = [
     # WDB (monthly)
     {'WellName':'WellName','ReportDate':'ReportDate','TotalOil':'TotalOil',
      'TotalGas':'TotalGas','TotalWater':'TotalWater','API10':'API'},
     # DI / IHS (monthly)
     {'WellName':'Well Name','ReportDate':'Monthly Production Date','TotalOil':'Monthly Oil',
-     'TotalGas':'Monthly Gas','TotalWater':'Monthly Water','API10':'API10'},
-    # Generic daily format
-    {'WellName':'WellName','ReportDate':'Date','TotalOil':'DailyOil',
-     'TotalGas':'DailyGas','TotalWater':'DailyWater','API10':'API'}
+     'TotalGas':'Monthly Gas','TotalWater':'Monthly Water','API10':'API10'}
 ]
-
 
 # --- Daily production column maps (common variants) ---
 DAILY_PROD_COLUMN_MAPS = [
